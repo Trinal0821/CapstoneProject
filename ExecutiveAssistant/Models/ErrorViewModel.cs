7c8bae2c3c1535fa1efe6e215c26399dd0ec7060
Trina Luong <u1117393@utah.edu>
2022-12-09 18:59:28 -0700

    basic setup

---
+++ ExecutiveAssistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExecutiveAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
