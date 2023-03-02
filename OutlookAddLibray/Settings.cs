4df4ade4c58b6a6f1b726cbd4f373d33165d30b2
Trina Luong <u1117393@gcloud.utah.edu>
2023-03-02 03:21:31 +0000

    Revert "Merge branch 'NLP-Complex' into 'main'"
    This reverts merge request !9

--- OutlookAddLibray/Settings.cs
+++ OutlookAddLibray/Settings.cs
@@ -17,6 +17,67 @@ using System.Threading.Tasks;
 ///                 unauthoized use of the code will be persecuted to the fullest 
 ///                 extent of the law. 
 /// </summary>
+namespace OutlookExecutable
+{
+    public class Settings
+    {
+        public Dictionary<string, Dictionary<string, int>> clientSettings { get; }
+        public Settings()
+        {
+            clientSettings = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, int> clientOne = new Dictionary<string, int>();
+            clientOne.Add("important", 500);
+            clientOne.Add("call", 20);
+            clientOne.Add("meet", 50);
+            clientOne.Add("talk", 5);
+            clientSettings.Add("Trina Luong", clientOne);
+            Dictionary<string, int> clientTwo = new Dictionary<string, int>();
+            clientTwo.Add("meet", 500);
+            clientTwo.Add("call", 2);
+            clientTwo.Add("cat", -5);
+            clientTwo.Add("ASAP", 200);
+            clientSettings.Add("Zach Gundersen", clientTwo);
+            Dictionary<string, int> clientThree = new Dictionary<string, int>();
+            clientThree.Add("important", 5);
+            clientThree.Add("up", 1);
+            clientThree.Add("meet", 1);
+            clientThree.Add("talk", 5);
+            clientSettings.Add("Harnassus", clientThree);
+
+        }
+        /// <summary>
+        /// Returns the "dictionary" attached to the client name. 
+        /// </summary>
+        /// <param name="clientName">The client that sent the email</param>
+        /// <returns></returns>
+        public Dictionary<string, int> GetCleintDictionary(string clientName)
+        {
+            clientSettings.TryGetValue(clientName, out Dictionary<string, int> result);
+            return result;
+        }
+    }
+}
+
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 
+/// This namespace contains the insides of our outlook add-on. 
+/// 
+/// 
+/// Authors: Team Executive Assistants:
+///           Zachary Gundersen, Trina loung, Andrew Gill, Sephora Batmean 
+/// 
+/// 2022-2023 Capstone Project, University of Utah
+/// 
+///*copyright note* This code is the property of Team Executive Assistants any 
+///                 unauthoized use of the code will be persecuted to the fullest 
+///                 extent of the law. 
+/// </summary>
 /// 
 namespace OutlookExecutable
 {
@@ -33,20 +94,16 @@ namespace OutlookExecutable
         public Dictionary<string,int> GetCleintDictionary(string clientName)
         {
             Dictionary<string,int> dict = new Dictionary<string, int>();
-
-            using (StreamReader reader = new StreamReader("C:..\\..\\..\\..\\OutlookAddLibray\\WordWeights.txt"))
+            string  filePath = @"C:\Client_Information\Client_Dictionary\" + clientName + ".txt";
+            
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            foreach (string line in lines)
             {
-                while (!reader.EndOfStream)
-                {
-
-                    string[] value = reader.ReadLine().Split("\t");
-                    if (value.Length > 1)
-                    {
-                        dict.Add(value[0].ToLowerInvariant(), Int32.Parse(value[1])); // find bug
-                    }
-                }
+                string[] value = line.Split("\t");
+                dict.Add(value[0], Int32.Parse(value[1]));
             }
             return dict;
         }
     }
 }
+*/
