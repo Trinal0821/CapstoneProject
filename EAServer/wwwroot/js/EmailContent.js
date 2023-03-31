dfc4e0750ca2992538de6e1b913822de4789db09
trina0821 <u1117393@utah.edu>
2023-03-30 21:35:12 -0600

    Got the OAuth WORKIng!!!

--- EAServer/wwwroot/js/EmailContent.js
+++ EAServer/wwwroot/js/EmailContent.js
@@ -1,281 +1,33 @@
-﻿(function () {
-    "use strict";
-
-    // The Office initialize function must be run each time a new page is loaded
-    Office.initialize = function (reason) {
-        $(document).ready(function () {
-
-          ///  sendData();
-            ImportantWordType();
-            BackButtonClick();
-            AddButtonClick();
+﻿Office.onReady(() => { 
+    // Ensure the Firebase SDK has been initialized before continuing.
+        firebase.initializeApp({
+            apiKey: "AIzaSyDVBPzpvxoABeSMrz_MKhSuO-b6BZm8MgA",
+            authDomain: "executiveassistant2023-949a8.firebaseapp.com",
+            projectId: "executiveassistant2023-949a8",
+            storageBucket: "executiveassistant2023-949a8.appspot.com",
+            messagingSenderId: "612446520648",
+            appId: "1:612446520648:web:42d1e1176dc364f2f49f7d",
+            measurementId: "G-379Z4W3P41"
         });
-    };
+    
+    const provider = new firebase.auth.OAuthProvider('microsoft.com');
+    provider.setCustomParameters({
+        prompt: 'consent',
+        tenant: 'common'
+    });
 
-    /*async function sendData() {
-        getFrom();
-    }
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        const credential = result.credential;
+        const accessToken = credential.accessToken;
+        console.log(`Access token: ${accessToken}`);
+        // Use the access token to call the Microsoft Graph API or other Microsoft APIs.
+    }).catch((error) => {
+        console.error(`Failed to authenticate user: ${error}`);
+    });
+});
 
-    async function getFrom() {
-        //Get the from and append the client's name
-        const msgFrom = Office.context.mailbox.item.from;
-        var fromField = msgFrom.displayName;
 
-        //Get the subject and append it
-        var subjectField = Office.context.mailbox.item.subject;
 
-        console.log("Got subject and from");
 
-        await Office.context.mailbox.item.body.getAsync(
-            "text",
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    var bodyField = result.value;
 
-                    console.log(bodyField);
 
-                    axios.get("/Home/testing", {
-                        params:
-                        {
-                            from: fromField,
-                            subject: subjectField,
-                            body: bodyField
-                        }
-                    })
-                        .then(res => {
-                            console.log(res.data);
-                            AssignTags(res.data);
-                        });
-                }
-                else {
-                    console.log(result.status);
-                }
-            }
-        )
-    }
-
-    //Assign the tag colors to the email
-    // Note: In order for you to successfully add a category, it must be in the mailbox categories master list.
-    function AssignTags(tagColor) {
-
-        Office.context.mailbox.masterCategories.getAsync(function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                const masterCategories = asyncResult.value;
-                if (masterCategories && masterCategories.length > 0) {
-                    const categoryToAdd = [tagColor];
-                    Office.context.mailbox.item.categories.addAsync(categoryToAdd, function (asyncResult) {
-                        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                            console.log(`Successfully assigned category '${categoryToAdd}' to item.`);
-                            alert("We've scanned through thousands of emails");
-                        } else {
-                            console.log("categories.addAsync call failed with error: " + asyncResult.error.message);
-                        }
-                    });
-                } else {
-                    console.log(
-                        "There are no categories in the master list on this mailbox. You can add categories using Office.context.mailbox.masterCategories.addAsync."
-                    );
-                }
-            } else {
-                console.error(asyncResult.error);
-            }
-        });
-    }*/
-
-    //Get the list of available tag colors and check if it's already in the master category list. If not add it.
-    function getMasterCategories() {
-
-        const map = new Map();
-        map.set("High Priority", false);
-        map.set("Medium Priority", false);
-        map.set("Low Priority", false);
-
-        Office.context.mailbox.masterCategories.getAsync(function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                const categories = asyncResult.value;
-                if (categories && categories.length > 0) {
-
-                    for (var i = 0; i < categories.length; i++) {
-                        if (categories[i].displayName === "High Priority") {
-                            map.set(categories[i].displayName, true);
-                        }
-                        else if (categories[i].displayName === "Medium Priority") {
-                            map.set(categories[i].displayName, true);
-                        }
-                        else if (categories[i].displayName === "Low Priority") {
-                            map.set(categories[i].displayName, true);
-                        }
-                    }
-
-                } else {
-                    console.log("There are no categories in the master list.");
-                }
-            } else {
-                console.error(asyncResult.error);
-            }
-
-            for (var i = 0; i < map.size; i++) {
-                if (map.get("High Priority") === false) {
-                    addMasterCategories("High Priority", Office.MailboxEnums.CategoryColor.Preset0);
-                    map.set(categories[i].displayName, true);
-                }
-                else if (map.get("Medium Priority") === false) {
-                    addMasterCategories("Medium Priority", Office.MailboxEnums.CategoryColor.Preset3);
-                    map.set(categories[i].displayName, true);
-                }
-                else if (map.get("Low Priority") === false) {
-                    addMasterCategories("Low Priority", Office.MailboxEnums.CategoryColor.Preset4)
-                    map.set(categories[i].displayName, true);
-                }
-            }
-        });
-    }
-
-    //Adds the tag colors to the master category
-    function addMasterCategories(name, tagColor) {
-        const masterCategoriesToAdd = [
-            {
-                displayName: name,
-                color: tagColor
-            }
-        ];
-
-        Office.context.mailbox.masterCategories.addAsync(masterCategoriesToAdd, function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-            } else {
-                console.log("masterCategories.addAsync call failed with error: " + asyncResult.error.message);
-            }
-        });
-    }
-
-    //Get the tag colors that have been set on the email. 
-    //NOTE: I was thinking of using this method to move the emails into the designated folders
-    function getCategories() {
-        Office.context.mailbox.item.categories.getAsync(function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                const categories = asyncResult.value;
-                if (categories && categories.length > 0) {
-                    console.log("Categories assigned to this item:");
-                    console.log(JSON.stringify(categories));
-                } else {
-                    console.log("There are no categories assigned to this item.");
-                }
-            } else {
-                console.error(asyncResult.error);
-            }
-        });
-    }
-
-    //Add the tags to the email
-    // Note: In order for you to successfully add a category,
-    // it must be in the mailbox categories master list.
-    function addCategories() {
-        Office.context.mailbox.masterCategories.getAsync(function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                const masterCategories = asyncResult.value;
-                if (masterCategories && masterCategories.length > 0) {
-                    // Grab the first category from the master list.
-                    //const categoryToAdd = [masterCategories[0].displayName];
-                    const categoryToAdd = ["High Priority"];
-                    Office.context.mailbox.item.categories.addAsync(categoryToAdd, function (asyncResult) {
-                        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                            console.log(`Successfully assigned category '${categoryToAdd}' to item.`);
-                        } else {
-                            console.log("categories.addAsync call failed with error: " + asyncResult.error.message);
-                        }
-                    });
-                } else {
-                    console.log(
-                        "There are no categories in the master list on this mailbox. You can add categories using Office.context.mailbox.masterCategories.addAsync."
-                    );
-                }
-            } else {
-                console.error(asyncResult.error);
-            }
-        });
-    }
-
-    function addWordWeightsIntial() {
-        //Get the div
-        var container = document.getElementById("addMore");
-
-        container.innerHTML = ` <div>
-            <p> Important words </p>
-            <textarea class="importantwords" rows="1" cols="10"> </textarea>
-            <p> Weight </p>
-            </div>`
-
-        //Create the dropdown and add elements to it
-        var select = document.createElement("select");
-        for (var i = 1; i <= 100; i++) {
-            var option = document.createElement("option");
-            option.value = i;
-            option.text = i;
-            select.appendChild(option);
-        }
-
-        container.appendChild(select);
-    }
-
-    function AddButtonClick() {
-        document.getElementById("addBtn").onclick = async () => {
-            //Get the div
-            var container = document.getElementById("addMore");
-
-            container.innerHTML += ` <div>
-            <p> Important words </p>
-            <textarea class="importantwords" rows="1" cols="10"> </textarea>
-            <p> Weight </p>
-            </div>`
-
-            //Create the dropdown and add elements to it
-            var select = document.createElement("select");
-            for (var i = 1; i <= 100; i++) {
-                var option = document.createElement("option");
-                option.value = i;
-                option.text = i;
-                select.appendChild(option);
-            }
-
-            container.appendChild(select);
-        }
-    }
-
-    function ImportantWordType() {
-        document.getElementById("submitBtn").onclick = async () => {
-
-            const companyNameSelected = document.getElementById('companyName').checked;
-
-            if (companyNameSelected) {
-                document.getElementById("selectedCompanyName").style.display = "block";
-            }
-            else {
-                document.getElementById("selectedClientName").style.display = "block";
-            }
-
-            //Add the word weight dropdown
-            addWordWeightsIntial();
-            document.getElementById("addBtn").style.display = "block"
-            document.getElementById("question").style.display = "none"
-            document.getElementById("back").style.display = "block"
-            document.getElementById("addMore").style.display = "block";
-
-        }
-    }
-
-    function BackButtonClick() {
-        document.getElementById("back").onclick = async () => {
-
-            //Hide the buttons and the divs
-            document.getElementById("back").style.display = "none"
-            document.getElementById("selectedCompanyName").style.display = "none";
-            document.getElementById("selectedClientName").style.display = "none";
-            document.getElementById("addMore").style.display = "none";
-            document.getElementById("addBtn").style.display = "none";
-
-            //document.getElementById("backClick").style.display = "none";
-            document.getElementById("question").style.display = "block"
-        }
-    }
-
-})();
