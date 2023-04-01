992bceee88910bbc2014d61212f71ec592089ef7
Andrew Gill <u1084670@gcloud.utah.edu>
2023-04-01 13:56:38 -0600

    got tagging working

--- EAServer/wwwroot/js/FunctionFile.js
+++ EAServer/wwwroot/js/FunctionFile.js
@@ -10,9 +10,9 @@
 };*/
 Office.initialize = function () {
     console.log("HELLO THERE")
-/*    // Register event handler for the button click
-    var button = document.getElementById("myButton");
-    button.onclick = onButtonClick;*/
+    /*    // Register event handler for the button click
+        var button = document.getElementById("myButton");
+        button.onclick = onButtonClick;*/
 };
 
 function addInformational() {
@@ -27,16 +27,125 @@ function addInformational() {
 
 // Helper function to add a status message to the info bar.
 function statusUpdate(icon, text) {
-  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
-    type: "informationalMessage",
-    icon: icon,
-    message: text,
-    persistent: false
-  });
+    Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+        type: "informationalMessage",
+        icon: icon,
+        message: text,
+        persistent: false
+    });
 }
 
 function defaultStatus(event) {
-  statusUpdate("icon16" , "Hello World!");
+    statusUpdate("icon16", "Hello World!");
+}
+
+async function tagEmail() {
+    // Ensure the Firebase SDK has been initialized before continuing.
+    firebase.initializeApp({
+        apiKey: "AIzaSyDVBPzpvxoABeSMrz_MKhSuO-b6BZm8MgA",
+        authDomain: "executiveassistant2023-949a8.firebaseapp.com",
+        projectId: "executiveassistant2023-949a8",
+        storageBucket: "executiveassistant2023-949a8.appspot.com",
+        messagingSenderId: "612446520648",
+        appId: "1:612446520648:web:42d1e1176dc364f2f49f7d",
+        measurementId: "G-379Z4W3P41"
+    });
+
+    const provider = new firebase.auth.OAuthProvider('microsoft.com');
+    provider.setCustomParameters({
+        tenant: 'common'
+    });
+    provider.addScope('Mail.Read');
+    provider.addScope('Mail.ReadWrite');
+    provider.addScope('Mail.ReadBasic');
+    provider.addScope('MailboxSettings.ReadWrite');
+
+
+    //provider.addScope('User.Read');
+    provider.addScope('openid');
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        const credential = result.credential;
+        const accessToken = credential.accessToken;
+        console.log(`Access token: ${accessToken}`);
+        // Use the access token to call the Microsoft Graph API or other Microsoft APIs.
+        getAllUnread(accessToken);
+    }).catch((error) => {
+        //fireBaseError = error;
+        console.error(`Failed to authenticate user: ${error}`);
+    });
+
+    function getAllUnread(authtoken) {
+        fetch("https://graph.microsoft.com/v1.0/me/messages", {
+            headers: {
+                Authorization: `Bearer ${authtoken}`
+            }
+        }
+        ).then((response) => {
+            console.log("return");
+            response.json().then((data) => {
+                const emails = data.value;
+                emails.forEach((email) => {
+                    console.log(email)
+
+                    axios.get("/Home/getTag", {
+                        params:
+                        {
+                            from: email.from.emailAddress.name,
+                            subject: email.subject,
+                            body: email.bodyPreview
+                        }
+                    })
+                        .then(res => {
+                            console.log(res.data);
+                            //signTags(res.data);
+                            console.log("request new tag");
+                            /*
+                            Office.context.mailbox.restApi("/me/messages/" + email.id + "/categories").post({ "categories": res.data }, function (asyncResult) {
+                                if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+                                    console.log(`Successfully assigned category ${res.data} to message with ID ${email.id}.`);
+                                } else {
+                                    console.log(`Failed to assign category '${res.data}' to message with ID '${email.id}'.Error message: ${ asyncResult.error.message }`);
+                                }
+                        });*/
+                            var tagColor;
+                            if (res.data == "High Priority")
+                                tagColor = "red"
+                            else if (res.data == "Medium Priority")
+                                tagColor = "yellow"
+                            else if (res.data == "Low Priority")
+                                tagColor = "green"
+                            else
+                                tagColor = "white"
+
+                            const updateUrl = `https://graph.microsoft.com/v1.0/me/messages/${email.id}`;
+                            const payload = {
+                                categories: [res.data]
+                            };
+                            fetch(updateUrl, {
+                                method: 'PATCH',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': `Bearer ${authtoken}`
+
+                                },
+                                body: JSON.stringify(payload)
+                            }).then((response) => {
+                                console.log("added tag");
+                                console.log(response);
+                            }).catch((error) => {
+                                console.error(`Failed to update categories: ${error}`);
+                            });
+                        });
+
+
+
+                });
+            });
+        });
+    };
+
+
 }
 
 async function getFrom() {
@@ -70,25 +179,45 @@ async function getFrom() {
                 })
                     .then(res => {
                         console.log(res.data);
-                        AssignTags(res.data);
-
-                        // Register an event handler that displays the notification message
-                        Office.context.mailbox.addHandlerAsync(Office.EventType.ItemChanged, function (event) {
-                            displayNotification().then(function () {
-                                event.completed();
-                            }).catch(function (error) {
-                                console.error(error);
-                                event.completed();
-                            });
+                        //signTags(res.data);
+                        console.log("request new tag");
+                        const updateUrl = `https://graph.microsoft.com/v1.0/me/messages/${email.id}`;
+                        fetch(updateUrl, {
+                            method: 'PATCH',
+                            headers: {
+                                Authorization: `Bearer ${authtoken}`,
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(res.data)
+                        }).then((response) => {
+                            console.log("added tag");
+                            console.log(response);
+                        }).catch((error) => {
+                            console.error(`Failed to update categories: ${error}`);
                         });
-                       
                     });
             }
-            else {
-                console.log(result.status);
-            }
-        }
-    )
+        })
+
+
+    /*
+    // Register an event handler that displays the notification message
+    Office.context.mailbox.addHandlerAsync(Office.EventType.ItemChanged, function (event) {
+        displayNotification().then(function () {
+            event.completed();
+        }).catch(function (error) {
+            console.error(error);
+            event.completed();
+        });
+    });
+   
+});
+}
+else {
+console.log(result.status);
+}
+}
+)*/
 }
 
 function displayNotification() {
@@ -96,7 +225,7 @@ function displayNotification() {
     Office.context.mailbox.item.notificationMessages.addAsync('notification', {
         type: 'progressIndicator',
         message: 'My add-in is working on your request...',
-       // icon: 'icon-16'
+        // icon: 'icon-16'
     });
 
     // Update the progress value as the operation progresses
@@ -138,30 +267,32 @@ function handleResult(result) {
     event.completed();
 }
 
+/*
 //Assign the tag colors to the email
 // Note: In order for you to successfully add a category, it must be in the mailbox categories master list.
 function AssignTags(tagColor) {
 
-    Office.context.mailbox.masterCategories.getAsync(function (asyncResult) {
+Office.context.mailbox.masterCategories.getAsync(function (asyncResult) {
+if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+const masterCategories = asyncResult.value;
+if (masterCategories && masterCategories.length > 0) {
+    const categoryToAdd = [tagColor];
+    Office.context.mailbox.item.categories.addAsync(categoryToAdd, function (asyncResult) {
         if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-            const masterCategories = asyncResult.value;
-            if (masterCategories && masterCategories.length > 0) {
-                const categoryToAdd = [tagColor];
-                Office.context.mailbox.item.categories.addAsync(categoryToAdd, function (asyncResult) {
-                    if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                        console.log(`Successfully assigned category '${categoryToAdd}' to item.`);
-                       // alert("We've scanned through thousands of emails");
-                    } else {
-                        console.log("categories.addAsync call failed with error: " + asyncResult.error.message);
-                    }
-                });
-            } else {
-                console.log(
-                    "There are no categories in the master list on this mailbox. You can add categories using Office.context.mailbox.masterCategories.addAsync."
-                );
-            }
+            console.log(`Successfully assigned category '${categoryToAdd}' to item.`);
+           // alert("We've scanned through thousands of emails");
         } else {
-            console.error(asyncResult.error);
+            console.log("categories.addAsync call failed with error: " + asyncResult.error.message);
         }
     });
+} else {
+    console.log(
+        "There are no categories in the master list on this mailbox. You can add categories using Office.context.mailbox.masterCategories.addAsync."
+    );
 }
+} else {
+console.error(asyncResult.error);
+}
+});
+}
+*/
